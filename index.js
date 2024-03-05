c580331e092b6f9b0c69ab6185736276505e6bdc
ibrahimdevop21 <ibrahimdevop21@gmail.com>
2024-03-05 23:16:34 +0200

    main navbar

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const port = 3333;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
