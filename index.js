7a916775eda4c1fa8d2418a0abf05745b3306dc7
ibrahimdevop21 <ibrahimdevop21@gmail.com>
2024-03-06 22:16:16 +0200

    page init and main carosil

--- index.js
+++ index.js
@@ -12,7 +12,28 @@ app.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
+app.get('/views/islam.ejs', (req, res) => {
+  res.render('islam.ejs');
+});
+
+app.get('/views/sufi.ejs', (req, res) => {
+  res.render('sufi.ejs');
+});
+
+app.get('/views/mag.ejs', (req, res) => {
+  res.render('mag.ejs');
+});
+
+app.get('/views/library.ejs', (req, res) => {
+  res.render('library.ejs');
+});
+
+app.get('/views/gallery.ejs', (req, res) => {
+  res.render('gallery.ejs');
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 // hom
+// m,.
